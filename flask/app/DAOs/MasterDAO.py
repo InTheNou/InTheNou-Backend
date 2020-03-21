eb49a01ccc774d5d3c6346f23d3e45078769eb06
Brian Rodriguez <brian.rodriguez17@upr.edu>
2020-03-21 09:41:28 -0400

    Merged Diego's oAuth changes in and renamed MasterDAO dependent variables accordingly

--- flask/app/DAOs/MasterDAO.py
+++ flask/app/DAOs/MasterDAO.py
@@ -5,6 +5,6 @@ class MasterDAO:
         connection_url = "dbname=%s user=%s password=%s host=%s port=%s" % (config.POSTGRES_DB,
                                                                     config.POSTGRES_USER,
                                                                     config.POSTGRES_PW,
-                                                                    config.POSTGRES_URL,
+                                                                    config.POSTGRES_HOSTNAME,
                                                                     config.POSTGRES_PORT)
         self.conn = psycopg2._connect(connection_url)
