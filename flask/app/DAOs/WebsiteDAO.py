7336523844d5f38fc3ad37d092ad5f811b0d3412
diego-amador <diego.amador@upr.edu>
2020-03-28 22:20:26 -0400

    Updated Website DAO to reflect DAO Diagram

--- flask/app/DAOs/WebsiteDAO.py
+++ flask/app/DAOs/WebsiteDAO.py
@@ -3,26 +3,45 @@ from psycopg2 import sql, errors
 
 
 class WebsiteDAO(MasterDAO):
-    
-    def removeWebsitesGivenServiceID(self,wid,sid):
-        """
-        """
+
+    def getWebsiteByID(self,wid):
+
         cursor = self.conn.cursor()
-        query = sql.SQL("update {table1} set isdeleted = True  "
-                        "where ( {pkey1} = %s AND {pkey2} = %s ) "
-                        "returning {pkey1} ,sid,isdeleted,wdescription ").format(
-            table1=sql.Identifier('servicewebsites'),
-            pkey1=sql.Identifier('wid'),
-            pkey2=sql.Identifier('sid'))
-        try:
-            cursor.execute(query, (int(wid), int(sid)))
-            result = cursor.fetchone()
-            self.conn.commit()
-        except errors.ForeignKeyViolation as e:
-            result = e
-        if result is None:
-            return None
-        return result[0]
+        query = sql.SQL("select {fields} from {table1} "
+                        
+                        "where {pkey1} = %s ").format(
+            fields=sql.SQL(',').join([
+                sql.Identifier('wid'),
+                sql.Identifier('url')
+              
+            ]),
+            table1=sql.Identifier('websites'),
+            
+            pkey1=sql.Identifier('wid'))
+            
+        cursor.execute(query, (int(wid),))
+        result = cursor.fetchone()
+        return result
+    
+    def getWebsitesByEventID(self, eid):
+        cursor = self.conn.cursor()
+        query = sql.SQL("select {fields} from {table1} "
+                        "natural join {table2} "
+                        "where {pkey1} = %s and {pkey2} = %s;").format(
+            fields=sql.SQL(',').join([
+                sql.Identifier('wid'),
+                sql.Identifier('url'),
+                sql.Identifier('wdescription')
+            ]),
+            table1=sql.Identifier('eventwebsites'),
+            table2=sql.Identifier('websites'),
+            pkey1=sql.Identifier('eid'),
+            pkey2=sql.Identifier('isdeleted'))
+        cursor.execute(query, (int(eid), False))
+        result = []
+        for row in cursor:
+            result.append(row)
+        return result
    
     def getWebsitesByServiceID(self, sid):
         cursor = self.conn.cursor()
@@ -46,26 +65,6 @@ class WebsiteDAO(MasterDAO):
             result.append(row)
         return result   
 
-    def getWebsitesByEventID(self, eid):
-        cursor = self.conn.cursor()
-        query = sql.SQL("select {fields} from {table1} "
-                        "natural join {table2} "
-                        "where {pkey1} = %s and {pkey2} = %s;").format(
-            fields=sql.SQL(',').join([
-                sql.Identifier('wid'),
-                sql.Identifier('url'),
-                sql.Identifier('wdescription')
-            ]),
-            table1=sql.Identifier('eventwebsites'),
-            table2=sql.Identifier('websites'),
-            pkey1=sql.Identifier('eid'),
-            pkey2=sql.Identifier('isdeleted'))
-        cursor.execute(query, (int(eid), False))
-        result = []
-        for row in cursor:
-            result.append(row)
-        return result
-    
     def createWebsite(self,url):
         """
         """
@@ -89,8 +88,7 @@ class WebsiteDAO(MasterDAO):
             
             result = [None, None]
         return result[0]
-    
-    
+          
     def insertWebsite(self, url, cursor):
         """Inserts a website into the website table DOES NOT COMMIT CHANGES TO DB.
         Parameters:
@@ -117,8 +115,54 @@ class WebsiteDAO(MasterDAO):
         else:
             result = [None, None]
         return result
-    
-    
+   
+    def addWebsitesToService(self, sid, wid,wdescription, cursor):
+        """
+        Relates the websites to the event. DOES NOT COMMIT CHANGES TO
+        DB.
+        Parameters:
+            sid: newly created Service ID.
+            wid: website IDs
+            cursor: createService method call connection cursor to database.
+        """
+        
+        cursor = cursor
+        query = sql.SQL("insert into {table1} "
+                        "({insert_fields}) "
+                        "values (%s, %s, %s, %s );").format(
+            table1=sql.Identifier('servicewebsites'),
+            insert_fields=sql.SQL(',').join([
+                sql.Identifier('sid'),
+                sql.Identifier('wid'),
+                sql.Identifier('wdescription'),
+                sql.Identifier('isdeleted')
+            ]))
+        cursor.execute(query, (sid, wid,wdescription,False))
+        return
+      
+    def addWebsitesToEvent(self, eid, wid,wdescription, cursor):
+        """
+        Relates the websites to the event. DOES NOT COMMIT CHANGES TO
+        DB.
+        Parameters:
+            eid: newly created Event ID.
+            wid: website IDs
+            wdescription: description of website
+            cursor: createEvent method call connection cursor to database.
+        """
+        cursor = cursor
+        query = sql.SQL("insert into {table1} "
+                        "({insert_fields}) "
+                        "values (%s, %s, %s);").format(
+            table1=sql.Identifier('eventwebsites'),
+            insert_fields=sql.SQL(',').join([
+                sql.Identifier('eid'),
+                sql.Identifier('wid'),
+                sql.Identifier('wdescription')
+            ]))
+        cursor.execute(query, (int(eid), int(wid), wdescription))
+        return
+        
     def insertWebsiteToService(self, sid, wid, wdescription):
         """
         Relates the websites to the event. DOES NOT COMMIT CHANGES TO
@@ -149,74 +193,26 @@ class WebsiteDAO(MasterDAO):
 
         return result
     
-    
-    def addWebsitesToService(self, sid, wid,wdescription, cursor):
+    def removeWebsitesGivenServiceID(self,wid,sid):
         """
-        Relates the websites to the event. DOES NOT COMMIT CHANGES TO
-        DB.
-        Parameters:
-            sid: newly created Service ID.
-            wid: website IDs
-            cursor: createService method call connection cursor to database.
         """
-        cursor = cursor
-        query = sql.SQL("insert into {table1} "
-                        "({insert_fields}) "
-                        "values (%s, %s, %s, %s );").format(
-            table1=sql.Identifier('servicewebsites'),
-            insert_fields=sql.SQL(',').join([
-                sql.Identifier('sid'),
-                sql.Identifier('wid'),
-                sql.Identifier('wdescription'),
-                sql.Identifier('isdeleted')
-            ]))
-        cursor.execute(query, (sid, wid,wdescription,False))
-        return
-    
-  
-    def addWebsitesToEvent(self, eid, wid,wdescription, cursor):
-        """
-        Relates the websites to the event. DOES NOT COMMIT CHANGES TO
-        DB.
-        Parameters:
-            eid: newly created Event ID.
-            wid: website IDs
-            wdescription: description of website
-            cursor: createEvent method call connection cursor to database.
-        """
-        cursor = cursor
-        query = sql.SQL("insert into {table1} "
-                        "({insert_fields}) "
-                        "values (%s, %s, %s);").format(
-            table1=sql.Identifier('eventwebsites'),
-            insert_fields=sql.SQL(',').join([
-                sql.Identifier('eid'),
-                sql.Identifier('wid'),
-                sql.Identifier('wdescription')
-            ]))
-        cursor.execute(query, (int(eid), int(wid), wdescription))
-        return
-
-
-    def getWebsiteByID(self,wid):
-
         cursor = self.conn.cursor()
-        query = sql.SQL("select {fields} from {table1} "
-                        
-                        "where {pkey1} = %s ").format(
-            fields=sql.SQL(',').join([
-                sql.Identifier('wid'),
-                sql.Identifier('url')
-              
-            ]),
-            table1=sql.Identifier('websites'),
-            
-            pkey1=sql.Identifier('wid'))
-            
-        cursor.execute(query, (int(wid),))
-        result = cursor.fetchone()
-        return result
-
+        query = sql.SQL("update {table1} set isdeleted = True  "
+                        "where ( {pkey1} = %s AND {pkey2} = %s ) "
+                        "returning {pkey1} ,sid,isdeleted,wdescription ").format(
+            table1=sql.Identifier('servicewebsites'),
+            pkey1=sql.Identifier('wid'),
+            pkey2=sql.Identifier('sid'))
+        try:
+            cursor.execute(query, (int(wid), int(sid)))
+            result = cursor.fetchone()
+            self.conn.commit()
+        except errors.ForeignKeyViolation as e:
+            result = e
+        if result is None:
+            return None
+        return result[0]
+   
 
 
 
