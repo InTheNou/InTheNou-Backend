7f4bd46b3ceabe5d37c089600bf4c37e1c2b7864
diego-amador <diego.amador@upr.edu>
2020-04-14 11:19:39 -0400

    -Added CORS headers to support credentials trhough cookies and accept the origin localhost:8080 for testing purposes

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask , redirect ,url_for, flash, render_template
+from flask_cors import CORS
 from flask_login import login_required, logout_user
 from .config import Config,DB_URI
 from .models import db, login_manager
@@ -7,6 +8,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask (__name__)
+
+
 #configure Database URL for Psycopg2
 app.config['SQLALCHEMY_DATABASE_URI'] =DB_URI
 #configure enviroment variables located in config file 
@@ -17,6 +20,6 @@ app.register_blueprint(blueprint, url_prefix="/login")
 #initialize FLASK-SQLAlchemy object and FLASK-Login object 
 db.init_app(app)
 login_manager.init_app(app)
-
+CORS(app,supports_credentials=True,origins='http://localhost:8080',vary_header=True,expose_headers=['Set-Cookie','Session'])
 
 from app.routes import user_info_routes,user_auth_routes,events_info_routes,services_info_routes
--- flask/app/routes/user_auth_routes.py
+++ flask/app/routes/user_auth_routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import Flask, g, flash, redirect, url_for, render_template, session, request, jsonify
-
+from flask_cors import CORS
 from flask_dance.contrib.google import make_google_blueprint, google
 from flask_dance.consumer.requests import OAuth1Session
 from flask_dance.consumer import oauth_before_login
@@ -73,9 +73,7 @@ def signup():
 
 @app.route("/App/login", methods=['POST'])
 def app_login():
-
     info = request.json
-   
     newAccount = False
     user_usub = info["id"]
 
@@ -98,7 +96,6 @@ def app_login():
     session['token']=info['access_token']
     flash("Successfully signed in.")
     # sessionDict = str(session)[20:-1]
-
     # # print(sessionDict)
     # # {'_fresh': True, '_id': '934028cbba09af9ef6c35734f503a02c84a5f9d54e92c85bd1b3c7b0eb9167791a93fe9cf0a6a57c1af31d4d319031a244a2514124fa970b7ebb39d06249737f', '_user_id': '2', 'token': 'ya29.a0Ae4lvC25jHQPYb40hlyWPdxeVpgE8lPKEhYURwbfNkWdfO-4z4joM3zZByq1UlFdXbjt5y40-qYGy3lClOL6ffCyWRIYIBfgbia-vKBpA5Aspd5LNNIueAJI-zlO04k-vPHYUxmP2r3imNF33avaI3Xe0-3jSS-yOrNV"}
     # cookie = encodeFlaskCookie(secret_key=os.getenv(
@@ -107,9 +104,11 @@ def app_login():
     # session['cookys'] = cookie
     # # print(decodeFlaskCookie(secret_key=os.getenv("FLASK_SECRET_KEY"), cookieValue=cookie))
 
-    response = {"uid": str(user.id)}
+    response = make_response({"uid": str(user.id)})
+    
+    response.headers['Session']=str(session)
 
-    return jsonify(response)
+    return (response)
 
 
 @oauth_before_login.connect
@@ -156,16 +155,18 @@ def app_home():
 @app.route("/Dashboard/login", methods=['GET'])
 # @login_required
 def dashboard_login():
-
-    try:
-        (current_user.id)
-        return UserHandler().getUserByID(int(current_user.id))
-
-    except:
+    # try:
+    #     (current_user.id)
+    #     return UserHandler().getUserByID(int(current_user.id))
+    # except:
         session['AppLogin'] = False
+        query = User.query.filter_by(email="kensy.bernadeau@upr.edu")
+        user = query.one()
         print('Session Defined as ' + str(session['AppLogin']))
-        return redirect(url_for("google.login"))
+        login_user(user)
         #flash ("No user found ")
+        
+        return redirect("https://localhost:8080/#/login/succeed?uid=3") 
         
 
 
