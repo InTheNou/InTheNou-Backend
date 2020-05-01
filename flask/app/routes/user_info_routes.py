4b08733f21ee613e04bbb121c80749688d3a0159
diego-amador <diego.amador@upr.edu>
2020-05-01 15:55:34 -0400

    -Added User route sphinx documentation

--- flask/app/routes/user_info_routes.py
+++ flask/app/routes/user_info_routes.py
@@ -15,6 +15,96 @@ def not_found(Error):
 @app.route("/API/App/Users/uid=<int:uid>", methods=['GET'])
 @user_role_required
 def getUserByID(uid):
+    """
+       .. py:decorator:: user_role_required
+
+       Uses :func:`~app.handlers.UserHandler.UserHandler.getUserByID`
+
+       Get User By ID
+
+       .. :quickref: User; Get User By by id
+
+       :param uid: User ID
+       :type uid: int
+       :return: JSON
+
+       **Example request**:
+
+       .. sourcecode:: http
+
+         GET /API/App/Users/uid=1 HTTP/1.1
+         Host: inthenou.uprm.edu
+         Accept: application/json
+
+       **Example response**:
+
+       .. sourcecode:: http
+
+         HTTP/1.1 200 OK
+         Vary: Accept
+         Content-Type: text/javascript
+
+         {
+            "ecreation": "2020-04-25 21:42:57.094493",
+            "ecreator": {
+                "display_name": "Diego Amador",
+                "email": "diego.amador@upr.edu",
+                "roleid": 3,
+                "type": "Professor",
+                "uid": 2
+            },
+            "edescription": "Meeting to discuss plans for integration phase.",
+            "eend": "2020-08-05 17:41:00",
+            "eid": 1,
+            "estart": "2020-08-05 15:41:00",
+            "estatus": "active",
+            "estatusdate": "2020-04-25 21:44:00.365692",
+            "etitle": "Alpha Code Team Meeting",
+            "photourl": "https://images.pexels.com/photos/256541/pexels-photo-256541.jpeg",
+            "room": {
+                "building": {
+                    "babbrev": "S",
+                    "bcommonname": "STEFANI",
+                    "bid": 1,
+                    "bname": "LUIS A STEFANI (INGENIERIA)",
+                    "btype": "Academico",
+                    "distinctfloors": [1,2,3,4,5,6,7],
+                    "numfloors": 7,
+                    "photourl": null
+                },
+                "photourl": null,
+                "raltitude": 50.04,
+                "rcode": "123A1",
+                "rcustodian": "naydag.santiago@upr.edu",
+                "rdept": "INGENIERIA ELECTRICA",
+                "rdescription": "CAPSTONE",
+                "rfloor": 1,
+                "rid": 56,
+                "rlatitude": 50.04,
+                "rlongitude": 50.04,
+                "roccupancy": 0
+            },
+            "tags": [
+                {"tid": 6,"tname": "ARTE"},
+                {"tid": 42,"tname": "FILO"},
+                {"tid": 54,"tname": "ICOM"},
+                {"tid": 64,"tname": "INSO"}
+            ],
+            "websites": [
+                {
+                    "url": "http://ece.uprm.edu/~fvega/",
+                    "wdescription": "Our clients webpage",
+                    "wid": 2
+                }
+            ]
+         }
+
+       :reqheader Cookie: Must contain session token to authenticate.
+       :resheader Content-Type: application/json
+       :statuscode 200: no error
+       :statuscode 403: User is not logged in.
+       :statuscode 404: Event does not exist
+       """
     if request.method == 'GET':
         return UserHandler().getUserByID(uid=uid)
 
