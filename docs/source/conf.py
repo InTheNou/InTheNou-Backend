7c447989e57840a9e28f3611ce4f89d53ad061b6
Brian Rodriguez <brian.rodriguez17@upr.edu>
2020-05-18 14:32:00 -0400

    Updated release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Alpha Code'
 author = 'Diego J. Amador Bonilla & Brian Rodriguez Badillo'
 
 # The full version, including alpha/beta/rc tags
-release = '1.1.0'
+release = '1.2.0'
 
 master_doc = 'index'
 # -- General configuration ---------------------------------------------------
